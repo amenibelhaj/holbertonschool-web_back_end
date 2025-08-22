cb5ab1229e6b16138ac72fa04eab4d009ada7141
amenibelhaj <blameni073@gmail.com>
2025-08-22 13:16:20 +0200

    es6 classes

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,55 +1,18 @@
-// my comment
-export default class HolbertonCourse {
-  constructor(name, length, students) {
-    // Validate types
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    if (!Array.isArray(students) || !students.every(s => typeof s === 'string')) {
-      throw new TypeError('Students must be an array of strings');
-    }
+import HolbertonCourse from './2-hbtn_course.js';
 
-    this._name = name;
-    this._length = length;
-    this._students = students;
-  }
+test('HolbertonCourse checks constructor types', () => {
+  // Name must be a string
+  expect(() => {
+    new HolbertonCourse(10, 20, ["Lucie", "Guillaume"]);
+  }).toThrow(TypeError);
 
-  // Getter & Setter for name
-  get name() {
-    return this._name;
-  }
+  // Length must be a number
+  expect(() => {
+    new HolbertonCourse('PHP', '20', ["Lucie", "Guillaume"]);
+  }).toThrow(TypeError);
 
-  set name(newName) {
-    if (typeof newName !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = newName;
-  }
-
-  // Getter & Setter for length
-  get length() {
-    return this._length;
-  }
-
-  set length(newLength) {
-    if (typeof newLength !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    this._length = newLength;
-  }
-
-  // Getter & Setter for students
-  get students() {
-    return this._students;
-  }
-
-  set students(newStudents) {
-    if (!Array.isArray(newStudents) || !newStudents.every(s => typeof s === 'string')) {
-      throw new TypeError('Students must be an array of strings');
-    }
-    this._students = newStudents;
-  }
-}
+  // Students must be an array of strings
+  expect(() => {
+    new HolbertonCourse('PHP', 20, ['Lucie', 42]);
+  }).toThrow(TypeError);
+});
