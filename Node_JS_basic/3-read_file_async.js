da666259221621e2d40864292829525eb9965da9
amenibelhaj <blameni073@gmail.com>
2025-09-07 14:45:41 +0200

    throw error

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.trim().split('\n');
+      lines.shift();
+
+      const studentsByField = {};
+      let totalStudents = 0;
+
+      for (const line of lines) {
+        if (line.trim() !== '') {
+          const [firstname, , , field] = line.split(',');
+
+          if (!studentsByField[field]) {
+            studentsByField[field] = [];
+          }
+
+          studentsByField[field].push(firstname);
+          totalStudents += 1;
+        }
+      }
+
+      console.log(`Number of students: ${totalStudents}`);
+      for (const [field, students] of Object.entries(studentsByField)) {
+        console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+      }
+
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
